742ca222bf90d4ae1dd030d62d30619c4b744321
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-08-14 03:10:51 +0600

    refactor: Update user routes to use '/register' instead of '/' for creating a user

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,7 +3,12 @@ import { usersRoutes } from '../modules/Users/users.routes';
 
 const router = express.Router();
 
-const moduleRoutes = [{ path: '/users', route: usersRoutes }];
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: usersRoutes,
+  },
+];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 export default router;
