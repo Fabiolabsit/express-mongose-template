08a00207cb3ed75023143e154c6c1376345ff049
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-08-15 21:44:32 +0600

    refactor: Add notFound middleware for handling 404 errors

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,24 @@ import express, { Application, Request, Response } from 'express';
 import path from 'path';
 import globalErrorHandler from './app/errors/globalErrorHandler';
 import routes from './app/routes';
+import notFound from './app/middleware/notFound';
+import requestLogger from './app/utils/logger/requestLogger';
 
 const app: Application = express();
+
 // cors
 app.use(cors());
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-// Set the path to the views directory
 app.set('views', path.join(__dirname, '../views'));
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Log the request
+app.use(requestLogger);
 
 // Application routes
 app.use('/', routes);
@@ -25,15 +30,15 @@ app.get('/', async (req: Request, res: Response) => {
   res.render('welcome', { port: process.env.PORT });
 });
 
-//Not found route
-app.use((req: Request, res: Response) => {
-  res.status(404).json({
-    statusCode: 404, // Not Found
-    data: null,
-    message: 'Resource not found',
-    success: false,
-  });
+//Health check route
+app.get('/health', async (req: Request, res: Response) => {
+  res.status(200).json({ message: 'API is running' });
 });
+
+
+
+//Not found route
+app.use(notFound);
 
 // Error handling
 app.use(globalErrorHandler);
