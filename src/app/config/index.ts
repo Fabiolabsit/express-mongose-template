0ba7ed324979ba64a834ad7ecc758a8df4562260
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-08-15 21:44:37 +0600

    chore: Update default port to 5000 and add environment variables to config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,8 +7,13 @@ dotenv.config({ path: envPath });
 
 // Export the configuration object
 export default {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
   mongoUri: process.env.MONGO_URI,
   dbName: process.env.DB_NAME,
   nodeEnv: process.env.NODE_ENV,
+  bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+  refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY,
 };
