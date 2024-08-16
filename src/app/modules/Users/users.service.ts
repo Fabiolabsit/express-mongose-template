55106ca77578bdfffc393ab096e2fb318f0c1381
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-08-16 21:45:40 +0600

    refactor: Update createUser function to include access and refresh token generation
    This commit refactors the createUser function in the users.service.ts file to include the generation of access and refresh tokens. The generateAccessAndRefreshToken function is added to generate the tokens using the user's ID. The tokens are then saved to the user object and returned in the response. Additionally, the password is removed from the response for security purposes.

--- src/app/modules/Users/users.service.ts
+++ src/app/modules/Users/users.service.ts
@@ -1,20 +1,53 @@
-import { Request } from 'express';
-import { User } from './users.interface';
+
+import CustomError from '../../errors/CusromError';
+import { IUser } from './users.interface';
 import Users from './users.model';
 
+// generate access and refresh token
+const generateAccessAndRefreshToken = async (userID: string) => {
+  try {
+    const user = await Users.findById(userID);
+    if (!user) {
+      throw new CustomError('User not found', 404);
+    }
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken; // Save the refresh token to the DB
+    await user?.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new CustomError(error.message, 500);
+    } else {
+      throw new CustomError('Something went wrong while Generating Token', 500);
+    }
+  }
+};
+
 const getUsers = async () => {
-  const result: User[] = await Users.find({});
+  const result: IUser[] = await Users.find({});
   return result;
 };
 
-const createUser = async (req: Request) => {
-  const { userName, role, email } = req.body;
-  const result: User = await Users.create({
-    userName: userName,
-    role: role,
-    email: email,
-  });
+
+
+const createUser = async (userData: IUser) => {
+  const user = await Users.create(userData);
+  //generate access and refresh token
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id.toString());
+  
+  //remove the password from the response 
+  const result = {
+    ...user.toJSON(),
+    accessToken,
+    refreshToken,
+    
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  delete (result as any)?.password;
   return result;
 };
 
-export const userServices = { getUsers, createUser };
+export const userServices = { getUsers, createUser , generateAccessAndRefreshToken};
